260ba6600b5ac1de37caab8a64b14b5424fbf5e6
Dawiderter <dwalczak456@gmail.com>
2022-07-10 10:55:37 +0200

    tutorial

--- include/Scenes/MainMenuScene.hpp
+++ include/Scenes/MainMenuScene.hpp
@@ -11,6 +11,11 @@ public:
     {
         playButton = Node::create<Button>(std::string("play_sign.png"));
         playButton->wire_callback(el);
+        tutorialNotButton = Node::create<Button>(std::string("tutorial.png"));
+        playButton->setScale({4.f,4.f});
+        playButton->setTranslation({1000.f,20.f});
+        tutorialNotButton->setScale({3.2f,3.2f});
+        tutorialNotButton->setTranslation({1050.f,270.f});
     }
     virtual void draw() {
         Layers* layers = Layers::get_instance();
@@ -20,9 +25,8 @@ public:
         spr.setPosition({0.f,-30.f});
         (*layers)[0]->draw(spr);
 
-        playButton->setScale({4.f,4.f});
-        playButton->setTranslation({1000.f,20.f});
         playButton->draw();
+        tutorialNotButton->draw();
     }
     virtual void update(const sf::Time& delta) {
         animationManager.update(delta,false);
@@ -30,6 +34,7 @@ public:
     }
 private:
     std::shared_ptr<Button> playButton;
+    std::shared_ptr<Button> tutorialNotButton;
     EventListener<> el = EventListener<>([this]() {
         *curr_scene_ptr = playLevelScene;
     });
