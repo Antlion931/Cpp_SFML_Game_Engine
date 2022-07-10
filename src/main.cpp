5a0680347dcb32b2cbe0e49160fddec59dae5e98
Grubre <ogrodowczykjakub@gmail.com>
2022-07-10 03:16:39 +0200

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 #include "Nodes/Grid.hpp"
 #include "Standard/CenterString.hpp"
 #include "Nodes/smoke.hpp"
+#include "Scenes/MainMenuScene.hpp"
+#include "Scenes/PlayLevelScene.hpp"
 
 class game : public engine::engineer{
     using animation_map = std::unordered_map<std::string,AtlasManager::animation>;
@@ -30,16 +32,18 @@ class game : public engine::engineer{
         soundSystem = SoundSystem::getInstance("res/sounds/");
         musicSystem->playMusic("winter.wav");
         musicSystem->setRepeat("winter.wav", true);
-        spriteNode = Node::create<SpriteNode>();
-        warszawa = Node::create<Town>(center("Warszawa"), true, sf::Vector2f(500.0f, 500.0f));
-        berlin = Node::create<Town>(center("Berlin"), false, sf::Vector2f(350.0f, 500.0f));
-        train = Node::create<Train>();
-        animation_map m;
-        m["test"] = {1.f,{0,0},4};
-        atlasManager = new AtlasManager("tilesheet.png",{32,32},m);
+        //train = Node::create<Train>();
+        //animation_map m; m["test"] = {1.f,{0,0},4};
+        //atlasManager = new AtlasManager("tilesheet.png",{32,32},m);
 
-        grid = Node::create<Grid>(engine::Vec2i(34,33), std::string("tilesheet.png"), engine::Vec2i(32,32));
-        grid->scale({5.f,5.f});
+        mainMenuScene = new MainMenuScene();
+        playLevelScene = new PlayLevelScene();
+        curr_scene = (Scene*)mainMenuScene;
+
+        //grid = Node::create<Grid>(engine::Vec2i(5,5), std::string("tilesheet.png"), engine::Vec2i(32,32));
+        //grid->scale({5.f,5.f});
+        //grid->loadTileDataFromFile("");
+        //grid->setTile({0,0},5);
     }
     void update(const sf::Time& delta) override
     {
@@ -52,7 +56,6 @@ class game : public engine::engineer{
             { 
 
             }
-            
             if (event.type == sf::Event::KeyReleased)
             {
                 if(event.key.code == sf::Keyboard::Space)
@@ -65,68 +68,30 @@ class game : public engine::engineer{
                 //pl.append_vertex(sf::Vertex({(float)event.mouseButton.x,(float)event.mouseButton.y}, sf::Color(255,0,0,255)));
             }
         }
-        spriteNode->update(delta);
-        warszawa->update(delta);
-        berlin->update(delta);
-        train->update(delta);
-        atlasManager->update(delta);
-        grid->update(delta);
-
 
 
         // UPDATE VIEW (CAMERA FOLLOWS PLAYER)
-        sf::View new_view = window.getView();
+        /*sf::View new_view = window.getView();
         new_view.setCenter(train->getBodyTranslation());
         auto layers_vec = Layers::get_instance()->get_layers();
         for(int i = layers_vec.size()-1; i >= 0; i--)
             layers_vec[i]->setView(new_view);
-        ColorIDMap::get_instance()->get_color_layer()->setView(new_view);
+        ColorIDMap::get_instance()->get_color_layer()->setView(new_view);*/
+        curr_scene->update(delta);
     }
     void draw() override
     {
-        /*sf::VertexArray triangle(sf::Quads, 4);
-        // 35 x 30
-        // define the position of the triangle's points
-        sf::Vector2f a(10.f, 10.f), b(200.f, 10.f), c(200.f, 200.f), d(10.f, 200.f);
-
-        triangle[0].position = a;
-        triangle[1].position = b;
-        triangle[2].position = c;
-        triangle[3].position = d;
-
-        // define the color of the triangle's points
-        auto[x,y,z,w] = atlasManager->get_texture_coords_at(atlasManager->get_frame("test"));
-        triangle[0].texCoords = x;
-        triangle[1].texCoords = y;
-        triangle[2].texCoords = z;
-        triangle[3].texCoords = w;
-
-        sf::RenderStates state;
-
-        state.texture = atlasManager->get_texture().get();
-
-        (*layers)[1]->draw(triangle,state);*/
-        grid->draw();
-        //(*layers)[1]->draw(triangle);
-        
-
-        train->draw();
-        spriteNode->draw();
-        warszawa->draw();
-        berlin->draw();
-        train->draw();
+        curr_scene->draw();
     }
     private:
     // systems
     MusicSystem* musicSystem;
     SoundSystem* soundSystem;
-    std::shared_ptr<Train> train;
-    std::shared_ptr<SpriteNode> spriteNode;
-    std::shared_ptr<Town> warszawa;
-    std::shared_ptr<Town> berlin;
 
-    AtlasManager* atlasManager;
-    std::shared_ptr<Grid> grid;
+    MainMenuScene* mainMenuScene;
+    PlayLevelScene* playLevelScene;
+
+    Scene* curr_scene;
 };
 
 int main()
